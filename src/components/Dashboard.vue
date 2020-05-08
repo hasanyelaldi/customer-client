1779f7f7b643fa6433716e817206507289dc7938
Hasan Yelaldı <hasan.yelaldi@hititcs.com>
2020-05-09 00:24:10 +0300

    widgets added to dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,7 @@
 <template>
-    <v-row>
-        <v-col cols="6" sm="6">
+    <div>
+      <v-row>
+        <v-col cols="12" sm="6">
             <v-card class="mx-auto text-center" color="purple" dark max-width="600">
                 <v-card-text>
                     <v-sheet color="rgba(0, 0, 0, .12)">
@@ -10,7 +11,7 @@
                     </v-sheet>
                 </v-card-text>
                 <v-card-text>
-                    <div class="display-1 font-weight-thin">Count By Year</div>
+                    <div class="display-1 font-weight-thin">Customer Count By Year</div>
                 </v-card-text>
                 <v-divider></v-divider>
                 <v-card-actions class="justify-center">
@@ -18,17 +19,17 @@
                 </v-card-actions>
             </v-card>
         </v-col>
-        <v-col cols="6" sm="6">
+        <v-col cols="12" sm="6">
             <v-card class="mx-auto text-center" color="purple" dark max-width="600">
                 <v-card-text>
                     <v-sheet color="rgba(0, 0, 0, .12)">
-                        <v-sparkline :value="valuesOfCountByYear" :labels="labelsOfCountByYear" color="rgba(255, 255, 255, .7)" 
+                        <v-sparkline :value="valuesOfCountByCountry" :labels="labelsOfCountByCountry" color="rgba(255, 255, 255, .7)" 
                           height="100" padding="24" stroke-linecap="round" smooth>
                         </v-sparkline>
                     </v-sheet>
                 </v-card-text>
                 <v-card-text>
-                    <div class="display-1 font-weight-thin">Count By Year</div>
+                    <div class="display-1 font-weight-thin">Customer Count By Country</div>
                 </v-card-text>
                 <v-divider></v-divider>
                 <v-card-actions class="justify-center">
@@ -36,7 +37,70 @@
                 </v-card-actions>
             </v-card>
         </v-col>
-    </v-row>
+      </v-row>
+      <v-row>
+          <v-col cols="12" sm="3">
+            <v-card class="mx-auto text-center" color="purple" dark>
+                <v-card-text>
+                  <v-icon size="60">fa-user-circle</v-icon>
+                </v-card-text>
+                <v-card-text>
+                  <div class="display-1 font-weight-bold">{{ customerLength }}</div>
+                  <v-sheet color="rgba(0, 0, 0, .12)">
+                    <v-card-text>
+                      <div class="display-1 font-weight-thin">Customer</div>
+                    </v-card-text>
+                  </v-sheet>
+                </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <v-card class="mx-auto text-center" color="purple" dark>
+                <v-card-text>
+                  <v-icon size="60">fa-heart</v-icon>
+                </v-card-text>
+                <v-card-text>
+                  <div class="display-1 font-weight-bold">{{ activecustomerLength }}</div>
+                  <v-sheet color="rgba(0, 0, 0, .12)">
+                    <v-card-text>
+                      <div class="display-1 font-weight-thin">Active Customer</div>
+                    </v-card-text>
+                  </v-sheet>
+                </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <v-card class="mx-auto text-center" color="purple" dark>
+                <v-card-text>
+                  <v-icon size="60">fa-heartbeat</v-icon>
+                </v-card-text>
+                <v-card-text>
+                  <div class="display-1 font-weight-bold">{{ passivecustomerLength }}</div>
+                  <v-sheet color="rgba(0, 0, 0, .12)">
+                    <v-card-text>
+                      <div class="display-1 font-weight-thin">Passive Customer</div>
+                    </v-card-text>
+                  </v-sheet>
+                </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <v-card class="mx-auto text-center" color="purple" dark>
+                <v-card-text>
+                  <v-icon size="60">fa-university</v-icon>
+                </v-card-text>
+                <v-card-text>
+                  <div class="display-1 font-weight-bold">{{ countryLength }}</div>
+                  <v-sheet color="rgba(0, 0, 0, .12)">
+                    <v-card-text>
+                      <div class="display-1 font-weight-thin">Total Country</div>
+                    </v-card-text>
+                  </v-sheet>
+                </v-card-text>
+            </v-card>
+          </v-col>
+      </v-row>
+    </div>
 </template>
 
 
@@ -44,18 +108,23 @@
     import http from "../http-common";
     export default {  
     data: () => ({
-      value: [100, 22, 43, 8],
+      countries: [],
+      customers: [],
       customerCountByYears: [],
-      customers: []
+      customerCountByCountries: []
     }),
 
     created () {
-      this.retrieveCustomers();
+      this.retrieveCustomers()
     },
 
     watch: {
       customers(){
-        this.customerCountForYear();
+        this.customerCountForYear()
+        this.retrieveCountries()
+      },
+      countries(){
+        this.customerCountForCountry()
       }
     },
 
@@ -65,11 +134,29 @@
       },
       labelsOfCountByYear() {
         return this.customerCountByYears.map(x=>x.year);
+      },
+      valuesOfCountByCountry() {
+        return this.customerCountByCountries.map(x=>x.count);
+      },
+      labelsOfCountByCountry() {
+        return this.customerCountByCountries.map(x=>x.country);
+      },
+      countryLength(){
+        return this.countries.length;
+      },
+      customerLength(){
+        return this.customers.length;
+      },
+      activecustomerLength(){
+        return this.customers.filter(customer => customer.active == true).length;
+      },
+      passivecustomerLength(){
+        return this.customers.filter(customer => customer.active == false).length;
       }
     },
 
     methods: {
-        retrieveCustomers() {
+      retrieveCustomers() {
         http
           .get("/customers")
           .then(response => {
@@ -81,11 +168,22 @@
           });
       },
 
+      retrieveCountries() {
+        http
+          .get("/countries")
+          .then(response => {
+            this.countries = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+      },
+
       customerCountForYear() {
           var customers = this.customers;
           var years = [];
           
-
           customers.forEach(customer => {
             var year = parseInt(customer.createdTime.slice(0,4));
             if(!years.includes(year)){
@@ -102,20 +200,37 @@
               customers.forEach(customer => {
                 if(parseInt(customer.createdTime.slice(0,4)) == targetYear ){
                   counter = counter + 1;
-                  //console.log(year + " ---> " + sum);
                 }
               });
               if(counter > 0){
-                //console.log(year + " ---> " + count);
                 customerCountByYear.year = targetYear;
                 customerCountByYear.count = counter;
-
-                console.log("object ---> " + customerCountByYear.year + "-" + customerCountByYear.count);
                 this.customerCountByYears.push(customerCountByYear);
               }
           });
+      },
+      customerCountForCountry(){
+        var customers = this.customers;
+        var countries = this.countries;
 
-          console.log(this.customerCountByYears);
+        countries.forEach(country => {
+          var counter = 0;
+          var customerCountByCountry = {
+            country: '',
+            count: 0
+          };
+          customers.forEach(customer => {
+            if(customer.address.country.id == country.id){
+              counter++;
+            }
+          });
+          
+          if(counter > 0){
+                customerCountByCountry.country = country.name;
+                customerCountByCountry.count = counter;
+                this.customerCountByCountries.push(customerCountByCountry);
+          }
+        });
       }
     }
   }
